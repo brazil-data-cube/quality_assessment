f22277b8eec6eb344776c093f7ed8ac9f388e2f3
Fronza <guilherme.fronza@gmail.com>
2020-05-07 09:00:56 -0300

    inserted find images functions

--- validation_tools/image_utils.py
+++ validation_tools/image_utils.py
@@ -1,10 +1,12 @@
 # This file is part of Brazil Data Cube Validation Tools.
 # Copyright (C) 2020.
-
+# Python Native
+import os
 # 3rd party
 import geoarray
 import numpy
 from osgeo import gdal
+
 
 
 def stack_virtual_raster(image_list, output=None):
@@ -46,3 +48,29 @@ def load_multband_geoarray(ds):
 
     del ds
     return geoArr
+
+
+def find_images(image_path, bands):
+    band_list = list()
+    listdir = os.listdir(image_path)
+    for band in bands:
+        for filename in listdir:
+            if band in filename:
+                band_list.append(filename)
+    return band_list
+
+
+def find_images_band(image_path, band):
+    '''This function works with an dir path and a band (.tif) to be returned in that folders and subfolders
+    retrieve a list of specific bands.
+    img_list retrieve strings band paths
+    band = band string that user need retrive. i.e.: red, nir, blue, B02, B01, etc.'''
+    ###Create the empty list
+    img_list = list()
+    ###for in the folders to retrieve specific band in path, subdirs and files - using os.walk
+    for path, subdirs, files in os.walk(image_path):
+        for name in files:
+            if name.endswith(band + '.tif'):
+                img_list.append(os.path.join(image_path,name))
+    return img_list
+
