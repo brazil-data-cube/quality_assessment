5a3d3678581f42c698be12ae03759046c7b94b20
marujore <rennanmarujo@gmail.com>
2020-06-24 14:00:56 -0300

    uses GDAL ds instead of img paths

--- validationtools/raster_comparison.py
+++ validationtools/raster_comparison.py
@@ -8,20 +8,19 @@ import gdal
 import numpy
 
 
-def raster_intersection(path1, path2, output_dir=None, output_name=None, nodata1=None, nodata2=None):
+def raster_intersection(ds1, ds2, nodata1=None, nodata2=None, output_name1=None, output_name2=None):
     """Perform image intersection of two rasters with different extent and projection.
         Args:
-            path1 (string) - path to image 1 (reference)
-            path2 (string) - path to image 2 (target)
-            output_dir (string) - path to output files
+            ds1 (GDAL dataset) - GDAL dataset of an image
+            ds2 (GDAL dataset) - GDAL dataset of an image
             nodata1 (number) - nodata value of image 1
             nodata2 (number) - nodata value of image 2
+            output_name1 (string) - path to output intersection of ds1
+            output_name2 (string) - path to output intersection of ds2
         Returns:
-            dict Scene with sentinel file path
+            dataset1 (GDAL dataset), dataset2 (GDAL dataset): intersection dataset1 and intersection dataset2.
     """
-    ### Open images
-    ds1 = gdal.Open(path1)
-    ds2 = gdal.Open(path2)
+    ###Setting nodata
     nodata = 0
     ###Check if images NoData is set
     if nodata2 is not None:
@@ -61,24 +60,40 @@ def raster_intersection(path1, path2, output_dir=None, output_name=None, nodata1
     ###Translate to same projection
     ds2c = gdal.Translate('', ds2w, format='MEM', projWin=[minx, maxy, maxx, miny], outputSRS=projection)
     ds2w = None
+    ds1c = gdal.Translate('', ds1, format='MEM', projWin=[minx, maxy, maxx, miny], outputSRS=projection)
+    ds1 = None
 
     ###Check if will create file on disk
-    if output_dir is not None:
+    if output_name1 is not None or output_name2 is not None:
         driver = gdal.GetDriverByName("GTiff")
+        if output_name1 is None:
+            output_name1 = 'intersection1.tif'
+        if output_name2 is None:
+            output_name2 = 'intersection2.tif'
     else:
         driver = gdal.GetDriverByName("MEM")
+        output_name1 = ''
+        output_name2 = ''
 
-    ###Create output datasource
-    dataset = driver.Create(os.path.join(output_dir, output_name), xsize, ysize, 1, gdal.GDT_Float32)
-    dataset.SetGeoTransform(geoTransform)
-    dataset.SetProjection(projection)
-    ###Setting nodata value
-    dataset.GetRasterBand(1).SetNoDataValue(nodata)
+    dataset1 = driver.Create(output_name1, xsize, ysize, 1, ds1c.GetRasterBand(1).DataType)
+    dataset1.SetGeoTransform(geoTransform)
+    dataset1.SetProjection(projection)
+    dataset1.GetRasterBand(1).SetNoDataValue(nodata) ###Setting nodata value
+    dataset1.GetRasterBand(1).WriteArray(ds1c.GetRasterBand(1).ReadAsArray())
 
-    return ds1, ds2c, dataset
+    dataset2 = driver.Create(output_name2, xsize, ysize, 1, ds2c.GetRasterBand(1).DataType)
+    dataset2.SetGeoTransform(geoTransform)
+    dataset2.SetProjection(projection)
+    dataset2.GetRasterBand(1).SetNoDataValue(nodata) ###Setting nodata value
+    dataset2.GetRasterBand(1).WriteArray(ds2c.GetRasterBand(1).ReadAsArray())
+
+    ds1c = None
+    ds2c = None
+
+    return dataset1, dataset2
 
 
-def raster_absolute_diff(path1, path2, output_dir=None, nodata1=None, nodata2=None):
+def raster_absolute_diff(ds1, ds2, nodata1=None, nodata2=None, output_file=None):
     """Perform image absolute difference (support different extent and projection).
         Args:
             path1 (string) - path to image 1 (reference)
@@ -87,24 +102,41 @@ def raster_absolute_diff(path1, path2, output_dir=None, nodata1=None, nodata2=No
             nodata1 (number) - nodata value of image 1
             nodata2 (number) - nodata value of image 2
         Returns:
-            dict Scene with sentinel file path
+            dataset (GDAL dataset): dataset containing absolute difference between ds1 and ds2.
     """
-    print('Calculating difference between {} and {}'.format(path1, path2))
-    output_name = os.path.basename(path1)[0:-4] + "__DIF__" + os.path.basename(path2)[0:-4] + '.tif'
-    ds1, ds2, dataset = raster_intersection(path1, path2, output_dir, output_name, nodata1, nodata2)
+    if output_file is None:
+        output_file = 'abs_diff.tif'
+    ds1_intersec, ds2_intersec = raster_intersection(ds1, ds2, nodata1, nodata2, None, None)
 
     ### Read bands with numpy to algebra
-    nodata = dataset.GetRasterBand(1).GetNoDataValue()
-    bandtar = numpy.array(ds1.GetRasterBand(1).ReadAsArray().astype(float))
+    nodata = ds1_intersec.GetRasterBand(1).GetNoDataValue()
+    bandtar = numpy.array(ds1_intersec.GetRasterBand(1).ReadAsArray().astype(float))
     fill_bandtar = numpy.where(bandtar == nodata)
-    bandref = numpy.array(ds2.GetRasterBand(1).ReadAsArray().astype(float))
+    bandref = numpy.array(ds2_intersec.GetRasterBand(1).ReadAsArray().astype(float))
     fill_bandref = numpy.where(bandref == nodata)
+
+    ### Get extent from ds1
+    projection = ds1.GetProjectionRef()
+    geoTransform = ds1.GetGeoTransform()
+    [cols, rows] = ds1.GetRasterBand(1).ReadAsArray().shape
 
     ds1 = None
     ds2 = None
     diff = numpy.abs(bandtar - bandref)
     diff[fill_bandtar] = nodata
     diff[fill_bandref] = nodata
+
+    ###Check if will create file on disk
+    if output_file is not None:
+        driver = gdal.GetDriverByName("GTiff")
+    else:
+        driver = gdal.GetDriverByName("MEM")
+        output_file = ''
+
+    dataset = driver.Create(output_file, rows, cols, 1, ds1_intersec.GetRasterBand(1).DataType)
+    dataset.SetGeoTransform(geoTransform)
+    dataset.SetProjection(projection)
+    dataset.GetRasterBand(1).SetNoDataValue(nodata)
     dataset.GetRasterBand(1).WriteArray(diff)
 
     return dataset
